c717f1a0a9459ed45288fd4f6baf5739d5441a1b
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-04-16 19:29:40 -0400

    cool. for map reduce you have to do fields filtering by yourself...

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -4,7 +4,7 @@
 var mongorc = require('mongorc');
 var path = require('path');
 var pkg = require(__dirname + '/../package.json');
-mongorc.backup(function(){
+mongorc._init(function(){
   console.log('backing up current mongorc');
   mongorc.install(pkg.name, path.resolve(__dirname + '/../lib/mongo.js'), function(){
     console.log('installed to mongorc');
