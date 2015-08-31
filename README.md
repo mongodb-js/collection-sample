1b807b6b462339502920657a7d14268ee0738fb2
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-08-31 15:56:43 -0400

    refactoring to use $sample if available.

--- README.md
+++ README.md
@@ -1,41 +1,25 @@
-# mongodb-collection-sample
+# mongodb-collection-sample [![][npm_img]][npm_url] [![][travis_img]][travis_url]
 
-[![build status](https://secure.travis-ci.org/mongodb-js/mongodb-collection-sample.png)](http://travis-ci.org/mongodb-js/mongodb-collection-sample) 
-[![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/mongodb-js/mongodb-js?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
-
-
-Sample documents from a MongoDB collection.
+> Sample documents from a MongoDB collection.
 
 ## Install
 
-This module can be installed via the [node package manager (npm)](http://bit.ly/mongodb-js-setup).  
-Installing this module will modify your `~/.mongorc.js` so that when you start the `mongo` shell,
-a new `.sample()` function will be available for collections.
-
 ```
-npm install -g mongodb-collection-sample
+npm install --save mongodb-collection-sample
 ```
 
 ## Example
 
-After installing, start up `mongo` shell and try the following:
-
 ```javascript
-// Returns up to 5 documents from the user collection.
-db.users.sample()
 
-// Returns up to 4 documents from the user collection.
-db.users.sample(4)
-
-// Returns up to 3 documents from the user collection where username
-// starts with L.
-db.users.sample(3, {username: /^L/})
-
-// Returns up to 2 documents from the user collection where username
-// starts with L and only the username field.
-db.users.sample(2, {username: /^L/}, {username: 1})
 ```
 
 ## License
 
 Apache 2
+
+[travis_img]: https://secure.travis-ci.org/mongodb-js/mongodb-collection-sample.svg?branch=master
+[travis_url]: https://travis-ci.org/mongodb-js/mongodb-collection-sample
+[npm_img]: https://img.shields.io/npm/v/mongodb-collection-sample.svg
+[npm_url]: https://www.npmjs.org/package/mongodb-collection-sample
+[gitter_img]: https://badges.gitter.im/Join%20Chat.svg
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,149 @@
+var proxyquire = require('proxyquire');
+var assert = require('assert');
+var _range = require('lodash.range');
+var es = require('event-stream');
+var mongodb = require('mongodb');
+var sample = require('../');
+var ReservoirSampler = require('../lib/reservoir-sampler');
+var NativeSampler = require('../lib/native-sampler');
+
+var getSampler = function(version, fn) {
+  proxyquire('../lib', {
+    'get-mongodb-version': function(opts, cb) {
+      process.nextTick(function() {
+        cb(null, version);
+      });
+    }
+  }).getSampler({}, 'pets', {}, fn);
+};
+
 describe('mongodb-collection-sample', function() {
-  it.skip('should work', function() {
-    console.log('@todo: needs mongodb-runner that can do pretest and posttest hooks');
+  describe('polyfill', function() {
+    it('should use reservoir sampling if version is 3.1.5', function(done) {
+      getSampler('3.1.5', function(err, src) {
+        assert.ifError(err);
+        assert(src instanceof ReservoirSampler);
+        done();
+      });
+    });
+
+    it('should use native sampling if version is 3.1.6', function(done) {
+      getSampler('3.1.6', function(err, src) {
+        assert.ifError(err);
+        assert(src instanceof NativeSampler);
+        done();
+      });
+    });
+
+    it('should use native sampling if version is 3.1.7', function(done) {
+      getSampler('3.1.7', function(err, src) {
+        assert.ifError(err);
+        assert(src instanceof NativeSampler);
+        done();
+      });
+    });
+  });
+  describe('reservoir', function() {
+    it('should use `$natural: -1` as the default sort', function(done) {
+      getSampler('3.1.5', function(err, src) {
+        assert.ifError(err);
+        assert.deepEqual(src.sort, {
+          $natural: -1
+        });
+        done();
+      });
+    });
+  });
+
+  describe('functional', function() {
+    var db;
+
+    before(function(done) {
+      mongodb.MongoClient.connect('mongodb://localhost:27017/test', function(err, _db) {
+        if (err) return done(err);
+        db = _db;
+
+        var docs = _range(0, 1000).map(function(i) {
+          return {
+            _id: 'needle_' + i,
+            is_even: i % 2
+          };
+        });
+        db.collection('haystack').insert(docs, done);
+      });
+    });
+
+    after(function(done) {
+      if (!db) return done();
+      db.dropCollection('haystack', done);
+    });
+
+    it('should should default the sample size to `5`', function(done) {
+      var seen = 0;
+      sample(db, 'haystack')
+        .pipe(es.through(function(doc) {
+          seen++;
+          this.emit('data', doc);
+        },
+          function() {
+            this.emit('end');
+            assert.equal(seen, 5);
+            done();
+          }));
+    });
+
+    it('should allow specifying a query', function(done) {
+      var docs = [];
+      var options = {
+        size: 10,
+        query: {
+          is_even: 1
+        }
+      };
+      sample(db, 'haystack', options)
+        .pipe(es.through(function(doc) {
+          docs.push(doc);
+          this.emit('data', doc);
+        },
+          function() {
+            this.emit('end');
+            assert.equal(docs.filter(function(d) {
+              return d.is_even === 1;
+            }).length, options.size);
+            done();
+          }));
+    });
+
+    it('should get a sample of 10 documents', function(done) {
+      var seen = 0;
+      sample(db, 'haystack')
+        .pipe(es.through(function(doc) {
+          seen++;
+          this.emit('data', doc);
+        },
+          function() {
+            this.emit('end');
+            assert.equal(seen, 5);
+            done();
+          }));
+    });
+
+    it('should return as many documents as possible if '
+      + 'the requested sample size is larger than the '
+      + 'collection size', function(done) {
+        var seen = 0;
+        sample(db, 'haystack', {
+          size: 2000
+        })
+          .pipe(es.through(function(doc) {
+            seen++;
+            this.emit('data', doc);
+          },
+            function() {
+              this.emit('end');
+              assert.equal(seen, 1000);
+              done();
+            }));
+      });
   });
 });
